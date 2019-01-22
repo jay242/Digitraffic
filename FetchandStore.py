00479a11d0bce7f1f62973e82c009c528b4a4297
jay242 <jaijujoseph24@gmail.com>
2019-01-22 13:21:47 +0200

    new features and fixes.

--- FetchandStore.py
+++ FetchandStore.py
@@ -1,19 +1,20 @@
 import requests as re
 import sqlite3
+import  csv
 
 class FetchandStore:
+    sensors = []
 
-    @staticmethod
+    # @staticmethod
     def getData(link):
         data = re.get(link)
         jsondata = data.json()
-        sensors =[]
         for weatherstation in jsondata['weatherStations']:
             for sensorvalue in weatherstation["sensorValues"]:
-                sensors.append({"id": sensorvalue["roadStationId"], "name": sensorvalue["oldName"],
-                                   "value": sensorvalue["sensorValue"], "unit": sensorvalue["sensorUnit"],
-                                   "datetime":sensorvalue["measuredTime"]})
-        return sensors
+                FetchandStore.sensors.append({"id": sensorvalue["roadStationId"], "name": sensorvalue["oldName"],
+                                              "value": sensorvalue["sensorValue"], "unit": sensorvalue["sensorUnit"],
+                                              "datetime": sensorvalue["measuredTime"]})
+        return FetchandStore.sensors
 
     @staticmethod
     def storedataindb(sensors):
@@ -25,8 +26,9 @@ class FetchandStore:
         with connection:
             for sensor in sensors:
                 cursor.execute("INSERT INTO sensordata VALUES (:id, :name, :value, :unit, :time)",
-                               {'id':sensor['id'], 'name':sensor['name'],
-                                'value':sensor['value'], 'unit': sensor['unit'], 'time':sensor['datetime']})
+                               {'id': sensor['id'], 'name': sensor['name'],
+                                'value': sensor['value'], 'unit': sensor['unit'], 'time': sensor['datetime']})
+
     @staticmethod
     def fetchdatafromdb(sensorName):
         connection = sqlite3.connect('sensordata.db')
@@ -35,3 +37,32 @@ class FetchandStore:
         observedsensor = cursor.fetchall()
         return observedsensor
 
+    def storeroaddataindb(roadstationvalues):
+        connection = sqlite3.connect('roadstation.db')
+        cursor = connection.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS roadstation (
+                        ID int, NAME text, VALUE real, UNIT text, TIME text )"""
+                       )
+        with connection:
+            for roadstation in roadstationvalues:
+                cursor.execute("INSERT INTO roadstation VALUES (:id, :name, :value, :unit, :time)",
+                               {'id': roadstation['id'], 'name': roadstation['name'],
+                                'value': roadstation['value'], 'unit': roadstation['unit'],
+                                'time': roadstation['datetime']})
+                connection.commit()
+
+                #connection.close()
+
+    @staticmethod
+    def dbtocsv():
+        connection = sqlite3.connect("roadstation.db")
+        cursor = connection.cursor()
+        cursor.execute("Select * from roadstation")
+        roadstationdata = cursor.fetchall()
+
+        with open('roadstationdata.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['id','name','value','unit','time'])
+            writer.writerows(roadstationdata)
+
+#make one sensor readig as the dependent variable. All other sensors independent variable. Create columns like that.
--- WeatherStationData.py
+++ WeatherStationData.py
@@ -2,6 +2,9 @@ from FetchandStore import FetchandStore
 from DataFeatures import DataFeatures
 
 #Fetch the data from digitraffic website, parse the json and store relevant values in database
-FetchandStore.storedataindb(FetchandStore.getData("https://tie.digitraffic.fi/api/v1/data/weather-data"))
+#FetchandStore.storedataindb(FetchandStore.getData("https://tie.digitraffic.fi/api/v1/data/weather-data"))
 # Plot the first 100 values of the given sensor against time
-DataFeatures.processdata('airtemperature1')
+#DataFeatures.sensortypes(FetchandStore.sensors)
+#DataFeatures.processdata('airtemperature1')
+#DataFeatures.accumulate(3073)
+FetchandStore.dbtocsv()
